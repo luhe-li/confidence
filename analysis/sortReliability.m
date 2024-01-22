a78b2cf116b91619f849456ae3039173f9beb507
luhe-li <ll3981@nyu.edu>
2024-01-22 15:32:49 -0500

    sort sub1

--- analysis/sortReliability.m
+++ analysis/sortReliability.m
@@ -1,6 +1,6 @@
 clear;
 
-load('uniLoc_sub2_ses-V.mat');
+load('uniLoc_sub1_ses-V.mat');
 
 [~, temp1] = sort(VSinfo.SD_blob);
 reliSortResp = Resp(temp1);
@@ -12,4 +12,4 @@ sortedReli1Resp = reli1resp(temp2);
 [~, temp3] = sort([reli2resp.target_idx]);
 sortedReli2Resp = reli2resp(temp3);
 
-save('uniLoc_sub2_ses-V2.mat','Resp','reliSortResp','sortedResp','ExpInfo','ScreenInfo','VSinfo','AudInfo','sortedReli1Resp','sortedReli2Resp')
+save('uniLoc_sub1_ses-V2.mat','Resp','reliSortResp','sortedResp','ExpInfo','ScreenInfo','VSinfo','AudInfo','sortedReli1Resp','sortedReli2Resp')
--- analysis/uniLoc/s2_uni_bias_group.m
+++ analysis/uniLoc/s2_uni_bias_group.m
@@ -2,7 +2,7 @@ clear; clc; close all;
 
 %% set up
 
-sub_slc = 1;
+sub_slc = 2;
 
 % session
 ses_labels = {'-A','-V'};
