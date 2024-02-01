79b2ff79bb9f8f12237a38fa71a4bc9fcf16bc6c
zinongli <zl2242@nyu.edu>
2024-02-01 16:45:41 -0500

    Update transferAV.m

--- analysis/uniLoc/transferAV.m
+++ analysis/uniLoc/transferAV.m
@@ -1,58 +1,73 @@
 
-stds = NaN(3,8);
+nRep = Expinfo.nRep;
 targInds = unique([sortedResp.target_idx]);
 targDegs = unique([sortedResp.target_deg]);
-estA = NaN(8,20);
-estV1 = NaN(8,20);
-estV2 = NaN(8,20);
+targNum = length(targInds);
+rA = NaN(targNum,nRep);
+rV1 = NaN(targNum,nRep);
+rV2 = NaN(targNum,nRep);
 
-for i = 1:8
+for i = 1:targNum
     targInd = targInds(i);
     estDegsA = [sortedResp.response_deg];
     estDegsV1 = [sortedReli1Resp.response_deg];
     estDegsV2 = [sortedReli2Resp.response_deg];
 
-    estA(i,:) = estDegsA([sortedResp.target_idx] == targInd);
-    estV1(i,:) = estDegsV1([sortedReli1Resp.target_idx] == targInd);
-    estV2(i,:) = estDegsV1([sortedReli2Resp.target_idx] == targInd);
-
-    
+    rA(i,:) = estDegsA([sortedResp.target_idx] == targInd);
+    rV1(i,:) = estDegsV1([sortedReli1Resp.target_idx] == targInd);
+    rV2(i,:) = estDegsV2([sortedReli2Resp.target_idx] == targInd);
 end
 
+% %%
+% figure
+% subplot(1,3,1)
+% scatter(targDegs,rA,'filled','r')
+% hold on
+% plot(-30:30,-30:30,'--')
+% hold off
+% xlim([-30,30])
+% xlabel('Stimulus')
+% ylabel('Estimation')
+% title('A')
+% 
+% 
+% subplot(1,3,2)
+% scatter(targDegs,rV1,'filled','r')
+% hold on
+% plot(-30:30,-30:30,'--')
+% hold off
+% xlim([-30,30])
+% xlabel('Stimulus')
+% title('V1')
+% 
+% subplot(1,3,3)
+% scatter(targDegs,rV2,'filled','r')
+% hold on
+% plot(-30:30,-30:30,'--')
+% hold off
+% xlim([-30,30])
+% xlabel('Stimulus')
+% title('V2')
+% 
+% %%
+% scatter(rA(:),rV2(:),'filled','r')
+% xlim([-35,35])
+% ylim([-35,35])
+
 %%
-figure
-subplot(1,3,1)
-scatter(targDegs,estA,'filled','r')
-hold on
-plot(-30:30,-30:30,'--')
-hold off
-xlim([-30,30])
-xlabel('Stimulus')
-ylabel('Estimation')
-title('A')
+x = repmat(targDegs',1,nRep);
 
+mdlA = fitlm(x(:),rA(:));
+coefsA = table2array(mdlA.Coefficients(:,1));
+fitRA = x(:,1) .* coefsA(2) + coefsA(1);
 
-subplot(1,3,2)
-scatter(targDegs,estV1,'filled','r')
-hold on
-plot(-30:30,-30:30,'--')
-hold off
-xlim([-30,30])
-xlabel('Stimulus')
-title('V1')
+mdlV1 = fitlm(x(:),rV1(:));
+coefsV1 = table2array(mdlV1.Coefficients(:,1));
+fitSV1 = (fitRA - coefsV1(1)) ./ coefsV1(2);
 
-subplot(1,3,3)
-scatter(targDegs,estV2,'filled','r')
-hold on
-plot(-30:30,-30:30,'--')
-hold off
-xlim([-30,30])
-xlabel('Stimulus')
-title('V2')
-
-
-%%
-
+mdlV2 = fitlm(x(:),rV2(:));
+coefsV2 = table2array(mdlV2.Coefficients(:,1));
+fitSV2 = (fitRA - coefsV2(1)) ./ coefsV2(2);
 
 
 
