876a2953b770fd561b3d825e6c319e7100ac182c
luhe-li <114192631+luhe-li@users.noreply.github.com>
2024-03-03 20:10:21 -0500

    model fitting functions

--- modelFit/biLocV1/fitOptimalModel.m
+++ modelFit/biLocV1/fitOptimalModel.m
@@ -4,7 +4,7 @@ clear; clc; close all; rng('Shuffle');
 %% set environment
 
 useCluster                  = 0;
-sub = 1;
+sub = 2;
 ses = 1:2;
 
 % set cores
@@ -67,23 +67,26 @@ model.x                     = (-512:1:512) * deg_per_px;
 model.sA                    = ExpInfo.speakerLocVA(ExpInfo.audIdx);
 model.sV                    = model.sA;
 
-model.paraID                = {'\sigma_{A}','\sigma_{V1}','\sigma_{V2}','\sigma_{P}','p_{common}','criterion'};
+model.paraID                = {'aA','bA','\sigma_{A}','\sigma_{V1}','\sigma_{V2}','\sigma_{P}','p_{common}','criterion'};
 model.num_para              = length(model.paraID);
 model.num_rep               = size(data.org_resp, 5);
 
-
 % hard bounds, the range for LB, UB, larger than soft bounds
-paraH.sigA                  = [1e-2,    20]; % degree
-paraH.sigV1                 = [1e-2,    20]; % degree
-paraH.sigV2                 = [1e-2,    20]; % degree
+paraH.aA                    = [ 0.5,     2]; % degree
+paraH.bA                    = [  -8,     8]; % degree
+paraH.sigA                  = [1e-2,    10]; % degree
+paraH.sigV1                 = [1e-2,    10]; % degree
+paraH.sigV2                 = [1e-2,    10]; % degree
 paraH.sigP                  = [   1,    30]; % degree
 paraH.pC1                   = [1e-4,1-1e-4]; % weight
 paraH.c                     = [1e-4,1-1e-4]; % weight
 
 % soft bounds, the range for PLB, PUB
-paraS.sigA                  = [   5,    15]; % degree
-paraS.sigV1                 = [   5,    10]; % degree
-paraS.sigV2                 = [   5,    15]; % degree
+paraS.aA                    = [ 0.8,   1.2]; % degree
+paraS.bA                    = [  -4,     4]; % degree
+paraS.sigA                  = [   4,     6]; % degree
+paraS.sigV1                 = [   2,     6]; % degree
+paraS.sigV2                 = [   4,     6]; % degree
 paraS.sigP                  = [   1,    20]; % degree
 paraS.pC1                   = [ 0.4,   0.6]; % weight
 paraS.c                     = [ 0.2,   0.5]; % weight
@@ -104,7 +107,7 @@ OPTIONS.UncertaintyHandling = 0;
 %% model fitting
 
 % define function calculating nll
-funcNLL                     = @(p) nllOptimal(p(1), p(2), p(3), p(4), p(5), p(6),...
+funcNLL                     = @(p) nllOptimal(p(1), p(2), p(3), p(4), p(5), p(6),p(7), p(8),...
     model, data);
 
 % get grid initializations
@@ -113,31 +116,38 @@ model.init                  = getInit(model.lb, model.ub, numSections, model.num
 
 %initialize matrices that store negative log likelihood and best-fit paramters
 NLL                      = NaN(1, model.num_runs);
-estimatedP                  = NaN(model.num_runs, length(model.lb));
+estP                  = NaN(model.num_runs, length(model.lb));
 
 % test with a set of parameters if needed
-p = [ 1.6992    0.0122    0.0342   29.9916    0.2557    0.4714];
-testnll     = nllOptimal(p(1), p(2), p(3), p(4), p(5), p(6), model, data);
+p = [ 1.2, 4, 1.6992    0.0122    0.0342   29.9916    0.2557    0.4714];
+testnll     = nllOptimal(p(1), p(2), p(3), p(4), p(5), p(6), p(7), p(8), model, data);
 
 for i                    = 1:model.num_runs
     disp(i);
     try
         tempModel                   = model;
-        [estimatedP(i,:),NLL(i)] = bads(funcNLL, tempModel.init(i,:), tempModel.lb,...
+        [estP(i,:),NLL(i)] = bads(funcNLL, tempModel.init(i,:), tempModel.lb,...
             tempModel.ub, tempModel.plb, tempModel.pub, [], OPTIONS);
-        disp(estimatedP(i,:))
+        disp(estP(i,:))
     catch
         disp('Invalid NLL.')
         continue;
     end
 end
 
-model.estimatedP            = estimatedP;
-model.minNLL                = NLL;
-[~, best_idx] = min(NLL);
-bestP = estimatedP(best_idx, :);
+model.estP            = estP;
+model.NLL             = NLL;
+[model.minNLL, best_idx] = min(NLL);
+bestP = estP(best_idx, :);
+model.bestP = bestP;
+pred.bestP = bestP;
+
+%% predict using the best-fitting parameter
+
+p = bestP;
+pred = nllOptimal(p(1), p(2), p(3), p(4), p(5), p(6), p(7), p(8), model, data);
 
 % save the data for each participant
-flnm                        = sprintf('sub%d_batch%d', sub, idx_batch);
-save(['ModelFitResults_' flnm datestr(datetime('now'))],'data','model')
+flnm                        = sprintf('sub%d_ses%i-%i', sub, min(ses), max(ses));
+save(['ModelFitResults_' flnm datestr(datetime('now'))],'data','model','pred')
 
---
+++ modelFit/biLocV1/s2_compareModels.m
@@ -0,0 +1,7 @@
+models = {'Optimal',...
+    'Suboptimal',...
+    'Heuristic'};
+
+ heuristics = 5.2074e+03;
+ optimal = 5.2123e+03;
+ suboptimal = 5.2113e+03;
