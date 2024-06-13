84ff1037790d69526df51b8885cd76bf9da5b543
zinongli <zl2242@nyu.edu>
2024-06-13 19:34:54 -0400

    poster intermidieate

--- slides/IMRFabstract/posterAbstractPlots.m
+++ slides/IMRFabstract/posterAbstractPlots.m
@@ -9,9 +9,12 @@ sigma2 = 1.5; % standard deviation
 % Create the x range for the PDF
 x = linspace(mu1 - 4*sigma1, mu2 + 4*sigma2, 1000);
 
-% Calculate the PDFs
-pdf1 = normpdf(x, mu1, sigma1);
-pdf2 = normpdf(x, mu2, sigma2);
+% Calculate the PDFs and normalize
+pdfV = normpdf(x, mu1, sigma1);
+pdfV = pdfV ./ sum(pdfV);
+
+pdfA = normpdf(x, mu2, sigma2);
+pdfA = pdfA ./ sum(pdfA);
 
 % Calculate reliability (inverse of variance)
 reliability1 = 1 / sigma1^2;
@@ -21,40 +24,65 @@ reliability2 = 1 / sigma2^2;
 combined_mean = (reliability1 * mu1 + reliability2 * mu2) / (reliability1 + reliability2);
 combined_variance = 1 / (reliability1 + reliability2);
 combined_pdf = normpdf(x, combined_mean, sqrt(combined_variance));
+combined_pdf = combined_pdf ./ sum(combined_pdf);
 
-% Plot the PDFs
-figure;
-
-subplot(3,1,1);
-% Plot the grey shade under the first Gaussian curve
-fill([x, fliplr(x)], [combined_pdf, zeros(size(combined_pdf))], [0.8 0.8 0.8], 'EdgeColor', 'none');
-hold on;
+% Combination coefficient
+cc = 0.5;
 lw = 2;
-% Plot the Gaussian curves
-plot(x, pdf1, '-','Color','#B4222D','LineWidth',lw);
-plot(x, pdf2, '-','Color','#2270B0','LineWidth',lw);
+opac = 0.5;
+% Plot the PDFs
+
+figure;
+% Subplot 1
+subplot(3,1,1);
+fill([x, fliplr(x)], [combined_pdf, zeros(size(combined_pdf))],  ones(1,3) .* opac, 'EdgeColor', 'none');
+hold on;
+
+plot(x, pdfV, '--','Color','#B4222D','LineWidth',lw);
+plot(x, pdfA, '--','Color','#2270B0','LineWidth',lw);
 plot(x, combined_pdf, 'k-','LineWidth',lw);
 
-% Add labels and legend
-legend('','${m_V}$','${m_A}$','$\hat{s}$', 'Interpreter', 'latex');
-xlabel('Value');
-ylabel('Probability Density');
-title('Gaussian Distributions and Combined Distribution');
+% Add vertical lines
+plot([mu1, mu1], [0, max(pdfV)], '--','Color','#B4222D','LineWidth',lw);
+plot([mu2, mu2], [0, max(pdfA)], '--','Color','#2270B0','LineWidth',lw);
+plot([combined_mean, combined_mean], [0, max(combined_pdf)], 'k-','LineWidth',lw);
+xlim([-2.7,9]);
+set(gca, 'XTick', [], 'XTickLabel', [],'YTick', [], 'YTickLabel', []);
 hold off;
 
+% Subplot 2
 subplot(3,1,2);
-% Plot the grey shade under the first Gaussian curve
-fill([x, fliplr(x)], [pdf1, zeros(size(pdf1))], [0.8 0.8 0.8], 'EdgeColor', 'none');
+fill([x, fliplr(x)], [pdfA, zeros(size(pdfA))],  ones(1,3) .* opac, 'EdgeColor', 'none');
 hold on;
-lw = 2;
-% Plot the Gaussian curves
-plot(x, pdf1, '-','Color','#B4222D','LineWidth',lw);
-plot(x, pdf2, '-','Color','#2270B0','LineWidth',lw);
-plot(x, combined_pdf, 'k-','LineWidth',lw);
+plot(x, pdfV, '--','Color','#B4222D','LineWidth',lw);
+plot(x, pdfA, '-','Color','#2270B0','LineWidth',lw);
+plot(x, combined_pdf, 'k--','LineWidth',lw);
 
-% Add labels and legend
-legend('','${m_V}$','${m_A}$','$\hat{s}$', 'Interpreter', 'latex');
-xlabel('Value');
-ylabel('Probability Density');
-title('Gaussian Distributions and Combined Distribution');
+% Add vertical lines
+plot([mu1, mu1], [0, max(pdfV)], '--','Color','#B4222D','LineWidth',lw);
+plot([mu2, mu2], [0, max(pdfA)], '-','Color','#2270B0','LineWidth',lw);
+plot([combined_mean, combined_mean], [0, max(combined_pdf)], 'k--','LineWidth',lw);
+xlim([-2.7,9]);
+
+set(gca, 'XTick', [], 'XTickLabel', [],'YTick', [], 'YTickLabel', []);
+hold off;
+
+% Subplot 3
+subplot(3,1,3);
+post = pdfA .* (1-cc) + combined_pdf .* cc;
+post = post ./ sum(post);
+MAloc = mu2 .* (1-cc) + combined_mean .* cc;
+
+fill([x, fliplr(x)], [post, zeros(size(post))], ones(1,3) .* opac, 'EdgeColor', 'none');
+hold on;
+plot(x, post, '-','Color','#702963','LineWidth',lw);
+plot(x, pdfA, '--','Color','#2270B0','LineWidth',lw);
+plot(x, combined_pdf, 'k--','LineWidth',lw);
+
+plot([mu2, mu2], [0, max(pdfA)], '--','Color','#2270B0','LineWidth',lw);
+plot([combined_mean, combined_mean], [0, max(combined_pdf)], 'k--','LineWidth',lw);
+plot([MAloc, MAloc], [0, max(post).* 1.5], '-','Color','#702963','LineWidth',lw);
+xlim([-2.7,9]);
+title('$\sigma^2$', 'Interpreter', 'latex')
+set(gca, 'XTick', [], 'XTickLabel', [],'YTick', [], 'YTickLabel', []);
 hold off;
